e71ba785d23c428532f10e61ca631f73d3d39091
jeromefong <jerome.fong@ncs.com.sg>
2019-11-19 14:03:55 +0800

    fetch success

--- App.js
+++ App.js
@@ -3,40 +3,57 @@ import { Text, View, StyleSheet, Button } from 'react-native';
 import Constants from 'expo-constants';
 
 export default class App extends React.Component {
-
   constructor() {
     super();
     this.state = {
       TextHolder: 'Helo',
+      res_id: ''
     };
   }
 
-  handleClick = async () => {
-    try {
-      let response = await fetch(
-        'https://facebook.github.io/react-native/movies.json'
-      );
-      let responseJson = await response.json();
+  handleClick = () => {
 
-      this.setState({ TextHolder: 'hey bitchg' });
-      // this.state.TextHolder = this.state.TextHolder.toString().replace(responseJson);
-    } catch (error) {
-      console.error(error);
-    }
+    let longitude = 103.8198;
+    let latitude = 1.3521;
+    fetch(
+      'https://developers.zomato.com/api/v2.1/geocode?lat=1.3521&lon=103.8198',
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'user-key': '5889a1e617da464e2d2d6cd14e133409',
+        },
+      }
+    )
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({ res_id: responseJson.popularity.nearby_res[Math.floor(Math.random() * responseJson.popularity.nearby_res.length)] });
+      });
+
+  fetch(
+      'https://developers.zomato.com/api/v2.1/restaurant?res_id='+this.state.res_id,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'user-key': '5889a1e617da464e2d2d6cd14e133409',
+        },
+      }
+    )
+      .then(response => response.json())
+      .then(responseJson => {
+        console.log(responseJson);
+      });
   };
 
   render() {
     return (
       <View style={styles.container}>
-
-        <Button
-          title="Press me"
-          onPress={this.handleClick} />
+        <Button title="Press me" onPress={this.handleClick} />
 
         <Text>Finding what to eat?</Text>
         <Text>{this.state.TextHolder}</Text>
       </View>
-
     );
   }
 }
